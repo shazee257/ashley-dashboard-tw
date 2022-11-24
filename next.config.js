7228f86b965ae5fc975628f3ab2119c441ad6a69
mshahzad257 <114944060+mshahzad257@users.noreply.github.com>
2022-11-24 20:58:22 +0500

    babelrc

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
+  // swcMinify: true,
   images: {
     domains: [
       'localhost',
       'ashley-api.herokuapp.com'
     ],
   },
-
-  // async redirects() {
-  //   return [{
-  //     source: '/',
-  //     destination: '/login',
-  //     permanent: true,
-  //   }]
-  // }
 }
 
 module.exports = nextConfig
